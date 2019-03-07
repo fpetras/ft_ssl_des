0f67670996c7d5074216e5fa52b5a18ee0449799
Fabian Petras <34797639+fpetras@users.noreply.github.com>
2019-03-07 17:00:01 +0100

    Add stdin reading for option -p

--- md5.c
+++ md5.c
@@ -6,22 +6,49 @@
 /*   By: fpetras <fpetras@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/07 14:38:48 by fpetras           #+#    #+#             */
-/*   Updated: 2019/03/07 14:45:10 by fpetras          ###   ########.fr       */
+/*   Updated: 2019/03/07 16:58:03 by fpetras          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ssl.h"
 
-int		ft_md5(int ac, char **av)
+static char	*read_stdin(void)
 {
-	int i;
+	int		ret;
+	char	buf[BUFF_SIZE + 1];
+	char	*file1;
+	char	*file2;
 
-	i = 0;
-	ft_printf("%d\n", ac);
-	while (i < ac)
+	file1 = ft_strnew(0);
+	while ((ret = read(0, &buf, BUFF_SIZE)) > 0)
 	{
-		ft_printf("%s\n", av[i]);
-		i++;
+		buf[ret] = '\0';
+		file2 = file1;
+		file1 = ft_strjoin(file2, buf);
+		free(file2);
+	}
+	return (file1);
+}
+
+int			ft_md5(int ac, char **av)
+{
+//	int i;
+//
+//	i = 0;
+//	ft_printf("%d\n", ac);
+//	while (i < ac)
+//	{
+//		ft_printf("%s\n", av[i]);
+//		i++;
+//	}
+	char *input;
+
+	(void)av;
+	if (ac == 0 || g_opts[OPT_P])
+	{
+		input = read_stdin();
+		g_opts[OPT_P] ? ft_printf("%s\n", input) : 0;
+		free(input);
 	}
 	return (0);
 }
