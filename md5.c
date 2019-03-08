e5bca7ca1529c5ee23f2596a5a860e931ef1daab
Fabian Petras <34797639+fpetras@users.noreply.github.com>
2019-03-08 14:58:51 +0100

    Add correct format outputting

--- md5.c
+++ md5.c
@@ -6,23 +6,45 @@
 /*   By: fpetras <fpetras@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/07 14:38:48 by fpetras           #+#    #+#             */
-/*   Updated: 2019/03/08 11:08:05 by fpetras          ###   ########.fr       */
+/*   Updated: 2019/03/08 14:43:17 by fpetras          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ssl.h"
 
-int			ft_md5(int ac, char **av)
+void			md5_hash(char *input)
 {
-	char *input;
+	(void)input;
+	ft_printf("a43c1b0aa53a0c908810c06ab1ff3967");
+}
 
-	(void)av;
-	if (ac == 0 || g_opts[OPT_P])
+static void		md5_formatted_hash(char *filename, char *input)
+{
+	if (!g_opts[OPT_Q])
 	{
-		input = read_stdin();
-		g_opts[OPT_P] ? ft_printf("%s\n", input) : 0;
-		free(input);
+		if (!g_opts[OPT_R])
+		{
+			if (filename)
+				ft_printf("MD5 (%s) = ", filename);
+			else
+				ft_printf("MD5 (\"%s\") = ", input);
+		}
 	}
+	md5_hash(input);
+	if (!g_opts[OPT_Q] && g_opts[OPT_R])
+	{
+		if (filename)
+			ft_printf(" %s\n", filename);
+		else
+			ft_printf(" \"%s\"\n", input);
+	}
+	else
+		ft_printf("\n");
+}
+
+/*
+static void		print_opts_and_args(int ac, char **av)
+{
 	//print options
 	ft_printf("options:\n");
 	g_opts[OPT_P] ? ft_printf("-p\n") : 0;
@@ -33,5 +55,38 @@ int			ft_md5(int ac, char **av)
 	ft_printf("args:\n");
 	for (int i = 0; i < ac; i++)
 		ft_printf("%s\n", av[i]);
+}
+*/
+
+int				ft_md5(int ac, char **av)
+{
+	int		i;
+	char	*input;
+
+	i = 0;
+	if (ac == 0 || g_opts[OPT_P])
+	{
+		if ((input = read_stdin()) == NULL)
+			return (-1);
+		g_opts[OPT_P] ? ft_printf("%s", input) : 0;
+		md5_hash(input);
+		ft_printf("\n");
+		free(input);
+	}
+//	print_opts_and_args(ac, av);
+	if (g_opts[OPT_S])
+	{
+		md5_formatted_hash(NULL, av[i]);
+		i++;
+	}
+	while (i < ac)
+	{
+		if ((input = read_file(av[i])) != NULL)
+		{
+			md5_formatted_hash(av[i], input);
+			free(input);
+		}
+		i++;
+	}
 	return (0);
 }
