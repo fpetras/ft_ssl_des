4568b6addd8128e9c263a06f6840dc728a7ee06d
Fabian Petras <34797639+fpetras@users.noreply.github.com>
2019-04-01 17:34:00 +0200

    Restructure

--- usage.c
+++ usage.c
@@ -6,11 +6,20 @@
 /*   By: fpetras <fpetras@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 12:04:59 by fpetras           #+#    #+#             */
-/*   Updated: 2019/03/29 15:33:08 by fpetras          ###   ########.fr       */
+/*   Updated: 2019/04/01 17:32:00 by fpetras          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ssl.h"
+
+int		print_usage(char **av)
+{
+	ft_dprintf(2, "usage: %s <command> [<options>] [<arg>...]\n", av[0]);
+	ft_dprintf(2, "\n%sCOMMANDS%s\n", WHITE, TEXT_RESET);
+	ft_dprintf(2, "     md5\n");
+	ft_dprintf(2, "     sha224 sha256 sha384 sha512 sha512224 sha512256\n");
+	return (EXIT_FAILURE);
+}
 
 int		print_hash_usage(char **av)
 {
@@ -22,14 +31,5 @@ int		print_hash_usage(char **av)
 	ft_dprintf(2, "     -q            quiet mode\n");
 	ft_dprintf(2, "     -r            reverse the format of the output\n");
 	ft_dprintf(2, "     -s <string>   print the sum of the given string\n");
-	return (EXIT_FAILURE);
-}
-
-int		print_usage(char **av)
-{
-	ft_dprintf(2, "usage: %s <command> [<options>] [<arg>...]\n", av[0]);
-	ft_dprintf(2, "\n%sCOMMANDS%s\n", WHITE, TEXT_RESET);
-	ft_dprintf(2, "     md5\n");
-	ft_dprintf(2, "     sha224 sha256 sha384 sha512 sha512224 sha512256\n");
 	return (EXIT_FAILURE);
 }
